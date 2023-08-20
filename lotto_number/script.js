77b94bf69428bd29e692017f57c2132f797e7a21
coolriver <longpaca@gmail.com>
2023-08-21 01:03:47 +0900

    중복 숫자 제거 부분 수정

--- lotto_number/script.js
+++ lotto_number/script.js
@@ -1,18 +1,24 @@
 const button = document.querySelector(".lottoBtn");
 const list = document.querySelector(".btnList");
 
-const lottoArray = [];
+const lottoArr = [];
 const createNumber = function () {
-  const randomNum = parseInt(Math.random() * 45 + 1);
-  if (!lottoArray.includes(randomNum)) {
-    lottoArray.push(randomNum);
+  while (lottoArr.length < 6) {
+    const randomNum = parseInt(Math.random() * 45 + 1);
+    if (!lottoArr.includes(randomNum)) {
+      lottoArr.push(randomNum);
+    }
   }
 };
 
+createNumber();
+console.log(lottoArr);
+
+let btnCount = 0;
 button.addEventListener("click", () => {
-  createNumber();
   const createBall = document.createElement("li");
-  createBall.innerText = lottoArray[lottoArray.length - 1];
+  createBall.innerText = lottoArr[btnCount];
+  btnCount++;
   list.appendChild(createBall);
   if (list.querySelectorAll("li").length === 6) {
     button.disabled = true;
