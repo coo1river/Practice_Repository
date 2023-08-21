f0ea2fdf3907e68993be739d15a74770a614895d
coolriver <longpaca@gmail.com>
2023-08-21 22:50:49 +0900

    reset 부분 추가

--- lotto_number/script.js
+++ lotto_number/script.js
@@ -1,26 +1,44 @@
 const button = document.querySelector(".lottoBtn");
 const list = document.querySelector(".btnList");
 
-const lottoArr = [];
 const createNumber = function () {
+  let lottoArr = [];
   while (lottoArr.length < 6) {
     const randomNum = parseInt(Math.random() * 45 + 1);
     if (!lottoArr.includes(randomNum)) {
       lottoArr.push(randomNum);
     }
   }
+  return lottoArr;
 };
 
-createNumber();
-console.log(lottoArr);
-
+let lottoArr;
 let btnCount = 0;
 button.addEventListener("click", () => {
+  if (list.querySelectorAll("li").length === 0) {
+    lottoArr = createNumber();
+  }
   const createBall = document.createElement("li");
-  createBall.innerText = lottoArr[btnCount];
-  btnCount++;
+  createBall.textContent = lottoArr[btnCount];
   list.appendChild(createBall);
+  btnCount++;
+
   if (list.querySelectorAll("li").length === 6) {
-    button.disabled = true;
+    button.textContent = "RESET";
+    button.classList.add("reset");
+
+    const reset = document.querySelector(".reset");
+
+    const fun = () => {
+      while (list.firstChild) {
+        list.firstChild.remove();
+      }
+
+      button.textContent = "PUSH";
+      button.classList.remove("reset");
+      btnCount = 0;
+      reset.removeEventListener("click", fun);
+    };
+    reset.addEventListener("click", fun);
   }
 });
